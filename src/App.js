cd72235b12c30fbd14ae3445a0cfe6dc3219d82e
Mahdi Samanbari <mahdisamanbari.it@gmail.com>
2024-10-14 19:27:36 +0330

    style added

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+    <nav>
+      <div className='logo'>
+        Mahdi Samanbari
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Services</li>
+      </ul>
+      <div className='menus'>
+        <i class="fa-solid fa-bars"></i>
+      </div>
+    </nav>
+      
+    </>
+  );
+};
+
+export default App;
